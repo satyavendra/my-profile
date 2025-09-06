a42883b7196f98572b8fa92057770d1f34ddc02c
satyavendra singh <satyavendra.singh@choicetechlab.com>
2025-09-06 15:28:02 +0530

    project setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Link } from "react-router-dom";
+// import Home from "./pages/Home";
+// import About from "./pages/About";
+// import Projects from "./pages/Projects";
+// import Contact from "./pages/Contact";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/projects">Tech Skills</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+        </ul>
+      </nav>
+
+      {/* Define routes */}
+      <Routes>
+        {/* <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} /> */}
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
+
+
+
+
