99a57c4d1904c8ddfa90e2fb56ba80d53a04ae9b
satyavendra singh <satyavendra.singh@choicetechlab.com>
2025-09-07 18:24:50 +0530

    work

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes } from "react-router-dom";
 import Navbar from './components/Navbar';
 import "../src/assets/css/common.scss";
+
 // import Home from "./pages/Home";
 // import About from "./pages/About";
 // import Projects from "./pages/Projects";
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,39 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react';
+import '../assets/css/common.scss';
 
-function Navbar() {
+const Navbar = () => {
   return (
-    <div className='custom-container'>
-        <nav>
-    <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About</Link></li>
-          <li><Link to="/projects">Tech Skills</Link></li>
-          <li><Link to="/contact">Contact</Link></li>
-        </ul>
-      </nav>
-    </div>
-  )
-}
+    <header className='header__wrapper'>
+      <div className="custom-container">
+        <div className="header__sec">
+          <div className="navbar__logo">
+            <h2>MyLogo</h2>
+          </div>
+          <nav className="navbar">
+            <div className="navbar__toggle">
+              <span className="bar"></span>
+              <span className="bar"></span>
+              <span className="bar"></span>
+            </div>
+            <ul className={`navbar__links`}>
+              <li>
+                <a href="#">Home</a>
+              </li>
+              <li>
+                <a href="#">About</a>
+              </li>
+              <li>
+                <a href="#">Services</a>
+              </li>
+              <li>
+                <a href="#">Contact</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </header>
+  );
+};
 
-export default Navbar
+export default Navbar;
